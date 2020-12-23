f53ee9e2f3cc3f3bf6f715d71eaf43c43b56bce6
Hasan Mohiuddin Ahmed <hasan_libra95@hotmail.com>
2020-12-23 23:12:49 +0500

    Completed calculate function logic.

--- calculator/calculator/views.py
+++ calculator/calculator/views.py
@@ -6,9 +6,29 @@ def home(request):
 
 
 def calculate(request):
-    number_1 = request.GET.get("number1")
-    number_2 = request.GET.get("number2")
-    operator = request.GET.get("operator")
-    data = {"test": f"{number_1} {operator} {number_2}"}
+    data = {
+        "answer": 0,
+        "error_message": "",
+        "expression": ""
+    }
 
-    return render(request, "home.html", data)
+    try:
+        number_1 = int(request.GET.get("number1"))
+        number_2 = int(request.GET.get("number2"))
+        operator = request.GET.get("operator")
+
+        if operator == "+":
+            data["answer"] = number_1 + number_2
+        elif operator == "-":
+            data["answer"] = number_1 - number_2
+        elif operator == "*":
+            data["answer"] = number_1 * number_2
+        elif operator == "/":
+            data["answer"] = number_1 / number_2
+
+        data["expression"] = str(number_1) + " " + operator + " " + str(number_2) + " = "
+
+        return render(request, "home.html", data)
+    except Exception as error:
+        data["error_message"] = f"Invalid Number ({str(error)})"
+        return render(request, "home.html", data)
